f5e4bf3fce37a48990ac4475d1e4eecb0c6fb213
mednouri123 <mednouri446@gmail.com>
2023-06-21 10:40:07 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int main() {
+int main() 
+{
     const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, message, strlen(message));
     return 1;
