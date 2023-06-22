a2270888bd0cb72652f33c33c2941bde91da33f6
mednouri123 <mednouri446@gmail.com>
2023-06-21 17:38:05 -0700

    putchar letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    char letter = 'a';
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
 
-    while (letter <= 'z') {
-        putchar(letter);
-        letter++;
-    }
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
